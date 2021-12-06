3dd0e8196db5bfc32b0a13abf9796316769f516c
whacksomber <meioliver00@gmail.com>
2021-12-06 12:31:53 -0800

    afds

---
+++ day6pt1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int numDays = 256;
+
+class fish {
+    int days;
+    bool isNew;
+
+    public:
+        void setDays(int x) {days = x;}
+        fish (int x) {setDays(x); isNew = false;} //constructor
+        fish (int x, bool a) {setDays(x); isNew = a;} //constructor
+        int getDays() { return days; }
+        bool newFish() {return isNew;}
+        void setNew(bool n) { isNew = n; }
+};
+
+void updateFish (vector<fish>&vec) {
+    for (int d = 0; d < numDays; d++) {
+        printf("%d \n", d);
+        //update number of fish after d days
+        for (int i = 0; i < vec.size(); i++) {
+            if (vec[i].getDays() == 0) {
+                fish newFish(8, true);
+                vec.push_back(newFish); //add new fish
+                vec[i] = 6;
+            }
+            else {
+                if (!vec[i].newFish()) {vec[i].setDays(vec[i].getDays()-1);}
+            }
+            if (vec[i].newFish()) {vec[i].setNew(false);} //make new fish not new
+        }
+    }
+}
+
+int main() {
+    ifstream inFile ("day6input.txt"); //open input file
+
+    string str;
+    getline(inFile, str);
+
+    stringstream ss (str);
+    string temp;
+    vector<fish>fishes; //vector of fish
+
+    while (getline(ss, temp, ',')) {
+        int d;
+        stringstream ss2; ss2 << temp; ss2 >> d;
+        fish tempFish (d);
+        fishes.push_back(tempFish);
+    }
+
+    updateFish(fishes);
+    cout << "answer: " << fishes.size() << '\n';
+}
